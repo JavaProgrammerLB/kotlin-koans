c3e972dda92d66d341e692061d8fc0a90cfe8db7
刘备 <admin@yitianyigexiangfa.com>
2018-09-18 11:32:12 +0800

    fix two task

--- src/ii_collections/n14FilterMap.kt
+++ src/ii_collections/n14FilterMap.kt
@@ -10,12 +10,27 @@ fun example1(list: List<Int>) {
 
 fun Shop.getCitiesCustomersAreFrom(): Set<City> {
     // Return the set of cities the customers are from
-    todoCollectionTask()
+//    todoCollectionTask()
+    val customers = this.customers
+    var cities:HashSet<City> = HashSet<City>()
+    for (customer in customers){
+        cities.add(customer.city)
+    }
+    return cities
 }
 
 fun Shop.getCustomersFrom(city: City): List<Customer> {
     // Return a list of the customers who live in the given city
-    todoCollectionTask()
+    // todoCollectionTask()
+    val customers = this.getSetOfCustomers()
+    var resultCustomer:ArrayList<Customer>  = ArrayList<Customer>()
+    for(customer in customers){
+        if (customer.city == city){
+            resultCustomer.add(customer)
+        }
+    }
+    return resultCustomer
+
 }
 
 
