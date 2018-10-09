bda6ff8c52cddd4156aad2c4c1896f26319afad1
刘备 <admin@yitianyigexiangfa.com>
2018-10-09 20:14:30 +0800

    fix 25 and 26

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,33 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int):Comparable<MyDate>{
+    override fun compareTo(other: MyDate): Int {
+        val otherYear = other.year
+        val otherMonth = other.month
+        val otherDayOfMonth = other.dayOfMonth
+        if(year > otherYear){
+            return 1
+        } else if(year == otherYear){
+            if(month > otherMonth){
+                return 1
+            } else if(month == otherMonth){
+                if(dayOfMonth == otherDayOfMonth){
+                    return 0
+                } else if(dayOfMonth > otherDayOfMonth){
+                    return 1
+                } else{
+                    return -1
+                }
+            }else{
+                return -1
+            }
+        } else{
+            return -1
+        }
+        return -1
+    }
+
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
@@ -10,4 +37,4 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate):ClosedRange<MyDate>
